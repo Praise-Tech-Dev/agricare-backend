9c173f6217e679000e36ead750f21b01dbd431e8
Praise-Tech-Dev <spraiseadeogun@gmail.com>
2026-05-28 22:53:00 +0100

    Fix: Configured global DRF permissions to allow public swagger access

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
