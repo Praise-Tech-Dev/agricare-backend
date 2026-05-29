c1e3a32c2511a7a04fa08170a403fec264aa25a1
Praise-Tech-Dev <spraiseadeogun@gmail.com>
2026-05-29 19:01:27 +0100

    Changed debug to true to see errors

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-key-for-local-dev-only")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'ripping-tibia-cannabis.ngrok-free.dev', 
